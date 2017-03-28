a1ce76ffff000946a97e0302c61c6c51b0414e35
colinfarrell <free.rbk@gmail.com>
2017-03-28 19:47:08 +0300

    before Import

--- Schedule.rb
+++ Schedule.rb
@@ -7,16 +7,17 @@ def action
 
 	puts "Что будем делать?"
 	a = gets.chomp
+		act = @ordnum.to_s + ". " + a
+
 	puts "Как долго будем #{a}? (в минутах)"
-	act = @ordnum.to_s + ". " + a
 	@time = gets.chomp.to_i*60
+
 	if @n == 0
 		@n = @t + @time #время окончания
 	else
 		@n = @time2+@time
 	end
 		@schedule[act] = @n
-		puts "#{act} до #{@n}"
 
 end
 	
@@ -25,7 +26,7 @@ loop do
 	@time2 = @n
 	
 	@ordnum += 1
-	puts "Продолжить? Любой символ, если 'Да', Enter, если закончили."
+	puts "Продолжить? Enter, если 'Да', любой другой символ + Enter, если закончили."
 	a = gets.chomp
 
 	if a == ""
