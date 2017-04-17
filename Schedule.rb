87a576a1c1a23143cb0a9ab973396bbaa4128522
svetarybalkina <free.rbk@gmail.com>
2017-04-17 18:51:21 +0300

    Saved

--- Schedule.rb
+++ Schedule.rb
@@ -3,45 +3,52 @@
 @ordnum = 1
 @n = 0
 
-def action #to complete schedule
+def add                             # to fill schedule
+                 
+  def write                         #add new string
 
-  puts "Что будем делать?"
-  dejstv = gets.chomp
-    act = @ordnum.to_s + ". " + dejstv
+    puts "Что будем делать?"
+    action = gets.chomp
+      act = @ordnum.to_s + ". " + action
 
-  puts "Сколько минут будем #{dejstv}?"
-  @time = gets.chomp.to_i*60
+    puts "Сколько минут будем #{action}?"
+    @time = gets.chomp.to_i*60
 
-  if @n == 0
-    @n = @t + @time #end time
-  else
-    @n = @time2+@time
-  end
+      if @n == 0                      # этот блок перенести в "запустить расписание"
+        @n = @t + @time               #end time of action
+      else
+        @n = @time2+@time
+      end
+
     @schedule[act] = @n
 
-  output = File.open "todo.txt", "a"
-  output.write @schedule
-  output.close
+    output = File.open "todo.txt", "a"
+    output.write @schedule
+    output.close
 
-end
-  
-loop do
-  puts 7.chr
-  action
-  @time2 = @n
-  
-  @ordnum += 1
-  puts "Продолжить? Enter, если продолжаем, Finish, если закончили."
-  b = gets.chomp
+  end
+    
+  loop do
+    puts 7.chr
+    write
+    @time2 = @n
+    
+    @ordnum += 1
+    puts "Продолжить? Enter, если продолжаем, f, если закончили."
+    b = gets.chomp
 
-  if b == 'Finish'
-    puts "Расписание составлено: "
-    puts @schedule
-    exit
+    if b == 'f'
+      puts "Расписание составлено: "
+      puts @schedule
+      exit
+    end
+
+    # input = File.open "todo.txt", "r"
+    # while (line = input.gets)
+    #   puts line
+    # end
   end
 
-  # input = File.open "todo.txt", "r"
-  # while (line = input.gets)
-  #   puts line
-  # end
 end
+
+add
