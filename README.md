df581e38b3091c884f3925127c5cf8aceed6d551
svetarybalkina <free.rbk@gmail.com>
2017-04-17 17:11:26 +0300

    Added mute

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Application for freelancers who started they working day in different time. 
+# Application for freelancers who started they working day in different time
 Schedule has slide time scale. 
 This application lets to start multihours schedule when you need. 
 With no link to definition time. 
 There will be some visual environment, alarm on gajets monitor and sound, 
 when task from the list finishes.
+
+To app sound on Linux Mint can need mpg321. To install: sudo apt install mpg321.
--- Schedule.rb
+++ Schedule.rb
@@ -3,45 +3,45 @@
 @ordnum = 1
 @n = 0
 
-def action
+def action #to complete schedule
 
-	puts "Что будем делать?"
-	dejstv = gets.chomp
-		act = @ordnum.to_s + ". " + dejstv
+  puts "Что будем делать?"
+  dejstv = gets.chomp
+    act = @ordnum.to_s + ". " + dejstv
 
-	puts "Как долго будем #{dejstv}? (в минутах)"
-	@time = gets.chomp.to_i*60
+  puts "Сколько минут будем #{dejstv}?"
+  @time = gets.chomp.to_i*60
 
-	if @n == 0
-		@n = @t + @time #время окончания
-	else
-		@n = @time2+@time
-	end
-		@schedule[act] = @n
+  if @n == 0
+    @n = @t + @time #end time
+  else
+    @n = @time2+@time
+  end
+    @schedule[act] = @n
 
-	output = File.open "todo.txt", "a"
-	output.write @schedule
-	output.close
+  output = File.open "todo.txt", "a"
+  output.write @schedule
+  output.close
 
 end
-	
+  
 loop do
-	puts 7.chr
-	action
-	@time2 = @n
-	
-	@ordnum += 1
-	puts "Продолжить? Enter, если 'Да', любой другой символ + Enter, если закончили."
-	b = gets.chomp
+  puts 7.chr
+  action
+  @time2 = @n
+  
+  @ordnum += 1
+  puts "Продолжить? Enter, если продолжаем, Finish, если закончили."
+  b = gets.chomp
 
-	if b == ""
-		puts "Расписание составлено: "
-		puts @schedule
-		exit
-	end
+  if b == 'Finish'
+    puts "Расписание составлено: "
+    puts @schedule
+    exit
+  end
 
-	# input = File.open "todo.txt", "r"
-	# while (line = input.gets)
-	# 	puts line
-	# end
+  # input = File.open "todo.txt", "r"
+  # while (line = input.gets)
+  #   puts line
+  # end
 end
