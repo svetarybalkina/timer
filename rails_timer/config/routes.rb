78ad1069ac7f9ab928db37c7d3957778cd00bd11
svetarybalkina <free.rbk@gmail.com>
2017-05-02 15:09:01 +0300

    Add contacts controller and new.html.erb view

--- rails_timer/config/routes.rb
+++ rails_timer/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
+  get 'home/index'
+
+  resource :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
