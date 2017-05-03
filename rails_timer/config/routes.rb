fbf88f03eaadd60f9002107a83dec2c564e229af
svetarybalkina <free.rbk@gmail.com>
2017-05-03 21:22:31 +0300

    Add articles

--- rails_timer/config/routes.rb
+++ rails_timer/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  resources :articles
+
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
