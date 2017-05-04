e477ecc1f6f338662b13b7f754970fd57c54820d
svetarybalkina <free.rbk@gmail.com>
2017-05-04 12:30:10 +0300

    Update articles#create

--- rails_timer/app/controllers/articles_controller.rb
+++ rails_timer/app/controllers/articles_controller.rb
@@ -5,9 +5,10 @@ class ArticlesController < ApplicationController
 
   def create
    @article = Article.new(article_params)
-   
+
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new'
     end
